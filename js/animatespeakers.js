cb42d8c10b3550943e560610a3e783494eda8139
Shubhang Sati <shubhang16198@iiitd.ac.in>
2017-10-22 18:18:20 +0530

    .

--- js/animatespeakers.js
+++ js/animatespeakers.js
@@ -1,12 +1,12 @@
 function animatespeakers(number, sw) {
-	var id = "speaker" + number;
+	var id = "#speaker" + number;
 	if (sw) {
 		var newsrc = "img/speakers/animated" + number + ".gif";
-		document.getElementById(id).src = newsrc;
+		$(id).attr('src', newsrc);
 	}
 	else {
 		var newsrc = "img/speakers/speaker" + number + ".jpg";
-		document.getElementById(id).src = newsrc;
+		$(id).attr('src', newsrc);
 	}
 
 }
