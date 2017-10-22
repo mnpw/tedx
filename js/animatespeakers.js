4455af5a35abd439b4c016768ad3a666eaa3161d
Shubhang Sati <shubhang16198@iiitd.ac.in>
2017-10-22 19:02:55 +0530

    .

--- js/animatespeakers.js
+++ js/animatespeakers.js
@@ -2,10 +2,12 @@ function animatespeakers(number, sw) {
 	var id = "#speaker" + number;
 	if (sw) {
 		var newsrc = "img/speakers/animated" + number + ".gif";
+		$(id).closest(".column").children()[0].style.backgroundColor = "transparent";
 		$(id).attr('src', newsrc);
 	}
 	else {
 		var newsrc = "img/speakers/speaker" + number + ".jpg";
+		$(id).closest(".column").children()[0].style.backgroundColor = "rgba(63, 173, 168, 0.3)";
 		$(id).attr('src', newsrc);
 	}
 
